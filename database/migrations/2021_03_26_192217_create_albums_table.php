a4c82d0cab28b15a569f2c3d599c30ae441fe954
Alan Ferreira <alanhfs@gmail.com>
2021-03-26 16:27:39 -0300

    Migration and Model for Albums

--- database/migrations/2021_03_26_192217_create_albums_table.php
+++ database/migrations/2021_03_26_192217_create_albums_table.php
@@ -15,6 +15,9 @@ class CreateAlbumsTable extends Migration
     {
         Schema::create('albums', function (Blueprint $table) {
             $table->id();
+            $table->string('artist');
+            $table->string('name');
+            $table->year('year');
             $table->timestamps();
         });
     }
